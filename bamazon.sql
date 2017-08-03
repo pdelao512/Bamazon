4ef5a5d637718f10720eae912424e7464b826e38
pdelao512 <pdelao512@gmail.com>
2017-08-03 15:28:27 -0500

    added modules

--- bamazon.sql
+++ bamazon.sql
@@ -1,40 +1,40 @@
-USE bamazon;
+USE Bamazon;
 
 CREATE TABLE Products (
-ItemID int NOT NULL,
-ProductName varchar(50) NOT NULL,
-DepartmentName varchar(50) NOT NULL,
-Price DECIMAL(5,2) NOT NULL,
-StockQuantity int NOT NULL);
+    ItemID INTEGER(11) AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price FLOAT(7, 2) NOT NULL,
+    StockQuantity INTEGER(7) NOT NULL,
+    PRIMARY KEY (ItemID)
+);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (23576, 'KitchenAid Mixer', 'Home', 314.99, 50);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('KitchenAid Mixer', 'Home', 314.99, 50);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (58026,'Amazon Fire TV', 'Electronics', 89.99, 200);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Amazon Fire TV', 'Electronics', 89.99, 200);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (51948, 'Samsung 50-inch 1080p Smart LED TV', 'Electronics', 479.00, 25);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Samsung 50-inch 1080p Smart LED TV', 'Electronics', 479.00, 25);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (38462, 'LEGO BATMAN MOVIE Batmobile Kit', 'Toys and Games', 139.95, 10);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('LEGO BATMAN MOVIE Batmobile Kit', 'Toys and Games', 139.95, 10);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (34958, 'Cards Against Humanity Game Set', 'Toys and Games', 29.99, 40);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Cards Against Humanity Game Set', 'Toys and Games', 29.99, 40);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (82469, 'Bic Round Medium Point Pens 35-Count', 'Office Supplies', 4.00, 400);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Bic Round Medium Point Pens 35-Count', 'Office Supplies', 4.00, 400);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (86735, 'Five Star Flex Black NoteBinder', 'Office Supplies', 15.77, 200);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Five Star Flex Black NoteBinder', 'Office Supplies', 15.77, 200);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (14368, 'Kershaw 1990X Brawler', 'Sports & Outdoors', 21.88, 60);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Kershaw 1990X Brawler', 'Sports & Outdoors', 21.88, 60);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (17593, 'Martini Golf Plastic Tees 5-Pack', 'Sports & Outdoors', 7.37, 230);
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Martini Golf Plastic Tees 5-Pack', 'Sports & Outdoors', 7.37, 230);
 
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity)
-VALUES (26947, 'My Little Pony Aluminum Cake Pan', 'Home', 15.25, 15);
-
-SELECT * FROM Products
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('My Little Pony Aluminum Cake Pan', 'Home', 15.25, 15);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,114 +1,330 @@
+//node module
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 var prompt = require('prompt');
-var colors = require('colors/safe');
+// var colors = require('colors/safe');
 var Table = require('cli-table');
+// var chalk = require('chalk');
+// var firstRun = require('first-run');
+// var figlet = require('figlet');
+
+//sql connection
 var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'bamazon',
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'Bamazon',
 });
 
-var productPurchased = [];
+//global variables
+var shoppingCart = [];
+var totalCost = 0;
 
-connection.connect();
-
-//connect to the mysql database and pull the information from the Products database to display to the user
-connection.query('SELECT ItemID, ProductName, Price FROM Products', function(err, result){
-	if(err) console.log(err);
-
-	//creates a table for the information from the mysql database to be placed
-	var table = new Table({
-		head: ['Item Id#', 'Product Name', 'Price'],
-		style: {
-			head: ['blue'],
-			compact: false,
-			colAligns: ['center'],
-		}
-	});
-
-	//loops through each item in the mysql database and pushes that information into a new row in the table
-	for (var i = 0; i < result.length; i++){
-		table.push(
-			[result[i].ItemID, result[i].ProductName, result[i].Price]
-		);
-	}
-	console.log(table.toString());
-
-	purchase();
+//connect to mysql and then run the main function
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    printItems(function() {
+        userSelectsItem();
+    });
 });
 
-//the purchase function so the user can purchase one of the items listed above
-var purchase = function(){
+//function to print all items to the console, uses npm module cli-table
+function printItems(cb) {
+    //new cli-table
+    var table = new Table({
+        head: ['ID Number', 'Product', 'Department', 'Price', 'Quantity Available']
+    });
+    //get all rows from the Products table
+    connection.query('SELECT * FROM Products', function(err, res) {
+        if (err) throw err;
+        //add all of the rows to the cli-table
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].ItemID, res[i].ProductName, res[i].DepartmentName, '$' + res[i].Price.toFixed(2), res[i].StockQuantity]);
+        }
+        //log the table to the console
+        console.log(table.toString());
+        //callback the userSelectsItems function to prompt the user to add items to cart
+        cb();
+    });
+}
 
-	//creates the questions that will be prompted to the user
-	var productInfo = {
-		properties: {
-			itemID:{description: colors.blue('Please enter the ID # of the item you wish to purchase!')},
-			Quantity:{description: colors.green('How many items would you like to purchase?')}
-		},
-	};
+//function to prompt users to add items to their cart
+function userSelectsItem() {
+    var items = [];
+    //get all product names from the Products table
+    connection.query('SELECT ProductName FROM Products', function(err, res) {
+        if (err) throw err;
+        //push all product names into the item array
+        for (var i = 0; i < res.length; i++) {
+            items.push(res[i].ProductName)
+        }
+        //prompt the user to select items from the items array
+        inquirer.prompt([{
+            name: 'choices',
+            type: 'checkbox',
+            message: 'Which products would you like to add to your cart? Press the space key to choose each Product and press enter when you are finished.',
+            choices: items
+        }]).then(function(user) {
+            //alert the user if they did not select anything and run function again
+            if (user.choices.length === 0) {
+                console.log('Oops you didn\'t select anything!');
+                //if the user doesn't select anything ask if they want to keep shopping or leave
+                inquirer.prompt([{
+                    name: 'choice',
+                    type: 'list',
+                    message: 'Your cart is empty. Would you like to continue shopping or exit?',
+                    choices: ['Continue Shopping', 'Exit']
+                }]).then(function(user) {
+                    //if keep shopping is selected print the items and prompt the user to select items again
+                    if (user.choice === 'Continue Shopping') {
+                        printItems(function() {
+                            userSelectsItem();
+                        });
+                    } else {
+                        //if leave is selected exit the program
+                        console.log('Thank You! Come Again!');
+                        connection.end();
+                    }
+                });
+            } else {
+                //run the howManyItems function with all of the items the user selected as an argument
+                howManyItems(user.choices)
+            }
+        });
+    });
+}
 
-	prompt.start();
+//function to prompt the user as to how many of each item they want
+function howManyItems(itemNames) {
+    //set item equal to the first element of the array and remove that element from the array
+    var item = itemNames.shift();
+    var itemStock;
+    var department;
+    //query mysql to get the current stock, price, and department of the item
+    connection.query('SELECT StockQuantity, Price, DepartmentName FROM Products WHERE ?', {
+        ProductName: item
+    }, function(err, res) {
+        if (err) throw err;
+        //set stock, price, and department in a variable
+        itemStock = res[0].StockQuantity;
+        itemCost = res[0].Price;
+        department = res[0].DepartmentName;
+    });
+    //prompt the user to ask how many of the item they would like
+    inquirer.prompt([{
+        name: 'amount',
+        type: 'text',
+        message: 'How many ' + item + ' would you like to purchase?',
+        //validate that the user input is a number and we have that much of the item in stock
+        validate: function(str) {
+            if (parseInt(str) <= itemStock) {
+                return true
+            } else {
+                //if we don't have that much in stock alert the user and ask for input again
+                console.log('\nOops! We only have ' + itemStock + ' of those in stock.');
+                return false;
+            }
+        }
+    }]).then(function(user) {
+        var amount = user.amount;
+        //create an object for the item and push it to the shoppingCart
+        shoppingCart.push({
+            item: item,
+            amount: amount,
+            itemCost: itemCost,
+            itemStock: itemStock,
+            department: department,
+            total: itemCost * amount
+        });
+        //if there are still items in the itemNames array run the function again
+        if (itemNames.length != 0) {
+            howManyItems(itemNames);
+        } else {
+            //if no items remain in the itemNames array run the checkout function
+            checkout();
+        }
+    });
+}
 
-	//gets the responses to the prompts above
-	prompt.get(productInfo, function(err, res){
+//function that allows user to edit shoppingCart before checkout and updating database
+function checkout() {
+    //ensure there are items in the shoppingCart
+    if (shoppingCart.length != 0) {
+        var grandTotal = 0;
+        //show the user all of the items in their shoppingCart
+        console.log('---------------------------------------------');
+        console.log('Here is your cart. Are you ready to checkout?');
+        for (var i = 0; i < shoppingCart.length; i++) {
+            var item = shoppingCart[i].item;
+            var amount = shoppingCart[i].amount;
+            var cost = shoppingCart[i].itemCost.toFixed(2);
+            var total = shoppingCart[i].total.toFixed(2);
+            var itemCost = cost * amount;
+            grandTotal += itemCost;
+            console.log(amount + ' ' + item + ' ' + '$' + total);
+        }
+        //show the total for the entire cart
+        console.log('Total: $' + grandTotal.toFixed(2));
+        //prompt the user if they are ready to checkout or need to edit the cart
+        inquirer.prompt([{
+            name: 'checkout',
+            type: 'list',
+            message: 'Ready to checkout?',
+            choices: ['Checkout', 'Edit Cart']
+        }]).then(function(res) {
+            //if the user is ready to checkout run the updateDB function to update database
+            if (res.checkout === 'Checkout') {
+                updateDB(grandTotal);
+            } else {
+                //if the user wants to edit the cart run the edit cart function
+                editCart();
+            }
+        });
+    } else {
+        //if the shoppingCart is empty alert the user and ask if they want to keep shopping or leave
+        inquirer.prompt([{
+            name: 'choice',
+            type: 'list',
+            message: 'Your cart is empty. Would you like to continue shopping or exit?',
+            choices: ['Continue Shopping', 'Leave']
+        }]).then(function(user) {
+            //if keep shopping is selected print the items and prompt the user to select items again
+            if (user.choice === 'Continue Shopping') {
+                printItems(function() {
+                    userSelectsItem();
+                });
+            } else {
+                //if leave is selected exit the program
+                console.log('Thank You! Come again!');
+                connection.end();
+            }
+        });
 
-		//places these responses in the variable custPurchase
-		var custPurchase = {
-			itemID: res.itemID,
-			Quantity: res.Quantity
-		};
+    }
+}
 
-		//the variable established above is pushed to the productPurchased array defined at the top of the page
-		productPurchased.push(custPurchase);
+//function to update the mysql database, takes grandTotal as an argument since it has already been totalled in checkout function
+function updateDB(grandTotal) {
+    //set the item to the first object in the shoppingCart array and remove that object from the array
+    var item = shoppingCart.shift();
+    var itemName = item.item;
+    var itemCost = item.itemCost
+    var userPurchase = item.amount;
+    var department = item.department;
+    var departmentTransactionSale = itemCost * userPurchase;
+    //query mysql to get the current total sales for the applicable department
+    connection.query('SELECT TotalSales from Departments WHERE ?', {
+        DepartmentName: department
+    }, function(err, res) {
+        var departmentTotalSales = res[0].TotalSales;
+        //update the department's TotalSales in the database
+        connection.query('UPDATE Departments SET ? WHERE ?', [{
+            TotalSales: departmentTotalSales += departmentTransactionSale
+        }, {
+            DepartmentName: department
+        }], function(err) {
+            if (err) throw err;
+        });
+    });
+    //query mysql to get the current StockQuantity of the item in case it has changed since the user has added the item to shoppingCart
+    connection.query('SELECT StockQuantity from Products WHERE ?', {
+        ProductName: itemName
+    }, function(err, res) {
+        var currentStock = res[0].StockQuantity;
+        //update the StockQuantity in the database
+        connection.query('UPDATE Products SET ? WHERE ?', [{
+            StockQuantity: currentStock -= userPurchase
+        }, {
+            ProductName: itemName
+        }], function(err) {
+            if (err) throw err;
+            //if there are still items in the shoppingCart run the function again
+            if (shoppingCart.length != 0) {
+                updateDB(grandTotal);
+            } else {
+                //if no items remain in the shoppingCart alert the user of the total and exit
+                grandTotal = grandTotal.toFixed(2);
+                console.log('Thank you for your purchase!');
+                console.log('Your total today was $' + grandTotal);
+                connection.end();
+            }
+        });
+    });
+}
 
-		//connects to the mysql database and selects the item the user selected above based on the item id number entered
-		connection.query('SELECT * FROM Products WHERE ItemID=?', productPurchased[0].itemID, function(err, res){
-				if(err) console.log(err, 'That item ID doesn\'t exist');
+//function to edit the shoppingCart
+function editCart() {
+    //push all product names of the items in the shoppingCart to an array
+    var items = [];
+    for (var i = 0; i < shoppingCart.length; i++) {
+        var item = shoppingCart[i].item;
+        items.push(item);
+    }
+    //prompt the user to select which items from the array they would like to edit
+    inquirer.prompt([{
+        name: 'choices',
+        type: 'checkbox',
+        message: 'Select the items you would like to edit.',
+        choices: items
+    }]).then(function(user) {
+        if (user.choices.length === 0) {
+            console.log('Oops! You didn\'t select anything to edit!');
+            checkout();
+        } else {
+            //run the editItem function and pass in the users selections as an argument
+            var itemsToEdit = user.choices;
+            editItem(itemsToEdit);
+        }
+    });
+}
 
-				//if the stock quantity available is less than the amount that the user wanted to purchase then the user will be alerted that the product is out of stock
-				if(res[0].StockQuantity < productPurchased[0].Quantity){
-					console.log('That product is out of stock!');
-					connection.end();
-
-				//otherwise if the stock amount available is more than or equal to the amount being asked for then the purchase is continued and the user is alerted of what items are being purchased, how much one item is and what the total amount is
-				} else if(res[0].StockQuantity >= productPurchased[0].Quantity){
-
-					console.log('');
-
-					console.log(productPurchased[0].Quantity + ' items purchased');
-
-					console.log(res[0].ProductName + ' ' + res[0].Price);
-
-					//this creates the variable SaleTotal that contains the total amount the user is paying for this total puchase
-					var saleTotal = res[0].Price * productPurchased[0].Quantity;
-
-					//connect to the mysql database Departments and updates the saleTotal for the id of the item purchased
-					connection.query("UPDATE Departments SET TotalSales = ? WHERE DepartmentName = ?;", [saleTotal, res[0].DepartmentName], function(err, resultOne){
-						if(err) console.log('error: ' + err);
-						return resultOne;
-					})
-
-					console.log('Total: ' + saleTotal);
-
-					//this variable contains the newly updated stock quantity of the item purchased
-					newQuantity = res[0].StockQuantity - productPurchased[0].Quantity;
-
-					// connects to the mysql database products and updates the stock quantity for the item puchased
-					connection.query("UPDATE Products SET StockQuantity = " + newQuantity +" WHERE ItemID = " + productPurchased[0].itemID, function(err, res){
-						// if(err) throw err;
-						// console.log('Problem ', err);
-						console.log('');
-						console.log(colors.cyan('Your order has been processed.  Thank you for shopping with us!'));
-						console.log('');
-
-						connection.end();
-					})
-
-				};
-
-		})
-	})
-
-};
+//function to edit individual items that the user selects to edit
+function editItem(itemsToEdit) {
+    //ensure that there are items to edit
+    if (itemsToEdit.length != 0) {
+        //set item to the first element in the array and remove that element from the array
+        var item = itemsToEdit.shift();
+        //ask the user if they would like to remove the item from the cart or edit the quantity
+        inquirer.prompt([{
+            name: 'choice',
+            type: 'list',
+            message: 'Would you like to remove ' + item + ' from your cart entirely or change the quantity?',
+            choices: ['Remove From Cart', 'Change Quanity']
+        }]).then(function(user) {
+            //if remove from cart is selected remove the item from the shoppingCart array
+            if (user.choice === 'Remove From My Cart') {
+                for (var i = 0; i < shoppingCart.length; i++) {
+                    if (shoppingCart[i].item === item) {
+                        shoppingCart.splice(i, 1);
+                        console.log('Updated!');
+                    }
+                }
+                //run the editItem function to check if there are more items to edit
+                editItem(itemsToEdit);
+            } else {
+                //if change quantity is selected ask the user what they would like to change the quantity to
+                inquirer.prompt([{
+                    name: 'amount',
+                    type: 'text',
+                    message: 'How many ' + item + ' would you like to purchase?',
+                }]).then(function(user) {
+                    //update the shoppingCart with the new quantity
+                    for (var i = 0; i < shoppingCart.length; i++) {
+                        if (shoppingCart[i].item === item) {
+                            shoppingCart[i].amount = user.amount;
+                            shoppingCart[i].total = shoppingCart[i].itemCost * user.amount;
+                            console.log('Updated!');
+                        }
+                    }
+                    //run the editItem function to check if there are more items to edit
+                    editItem(itemsToEdit);
+                });
+            }
+        });
+    } else {
+        //if no items remain to be edited run the checkout function
+        checkout();
+    }
+}
